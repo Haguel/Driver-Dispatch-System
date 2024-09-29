f1b7c8dee70b5e183388db0bc3d2dfe5c2de7f56
GlebHal <hammanov.gleb@gmail.com>
2024-09-29 10:47:49 +0300

    init project

---
+++ src/main/java/dev/haguel/dds/DriverDispatchSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.haguel.dds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverDispatchSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriverDispatchSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/haguel/dds/DriverDispatchSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.haguel.dds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverDispatchSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
